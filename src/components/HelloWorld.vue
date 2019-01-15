d730c952ab2549df35dc0e4c01b29cc405bd438a
Alexander Guevara <kesillo@Alexanders-Mac-mini.local>
2019-01-14 19:10:08 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,108 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="hello">
+		<div class="card-container">
+			<div class="card">
+				<div class="container-portada">
+					<div class="portada">
+					</div>
+				</div>
+				<div class="player"></div>
+				<div class="container-text">
+					<div class="text-one">
+						TE INVITO A MI
+					</div>
+					<div class="text-two">
+						BABYSHOWER
+					</div>
+				</div>
+				
+
+				<!-- <img src="../assets/bg_bomberman.png" alt=""> -->
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
+  bg_banner: require('../assets/bg_bomberman.png'),
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="scss">
+@font-face {
+    font-family: 'endless';
+    src: url('../fonts/EndlessBossBattle.ttf');
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.hello{
+	background-image: linear-gradient(45deg,#9f70b7, #7fddf7);
+	width: 100%;
+	min-height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.card-container{
+	min-width: 30rem;
+	background: black;
+	padding:40px;
+	box-sizing: border-box;
+	height: 500px;
 }
-a {
-  color: #42b983;
+.card{
+	/* background: white; */
+	position:relative;
+}
+.container-portada{
+	overflow: hidden;
+    position: relative;
+    height: 300px;
+}
+.portada{
+	// background: url('../assets/bg_bomberman.png');
+	background: url('https://cdn.dribbble.com/users/264259/screenshots/4598107/p1omegaattackbg.gif');
+    width: 100%;
+    height: 320px;
+    background-position: center;
+    background-size: cover;
+    position: absolute;
+    background-position-y: 37px;
+	transform: skewY(-12deg);
+    top: -64px;
+}
+.container-text{
+	font-family: endless;
+	position: absolute;
+    width: 100%;
+    transform: rotate(-12deg);
+    // background: orange;
+    top: 261px;
+	transform: skewY(-12deg);
+}
+.text-{
+	&one{
+		font-size: 30px;
+		color: #f5f5f5;
+		
+	}
+	&two{
+		// color: #fb8dcc;
+		color: #31dcff;
+		font-size: 60px;
+	}
+}
+.player{
+	background: url('../assets/bomberman.png');
+	top: 80px;
+    left: 8px;
+    position: absolute;
+    width: 270px;
+    height: 270px;
+    background-size: cover;
+    transform: scaleX(-1);
+    z-index: 9999;
 }
 </style>
