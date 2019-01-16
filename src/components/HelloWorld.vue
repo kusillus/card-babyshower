475fc8d13e4fa81371a56e86309bb17c5f59d240
Wilder Alexander Guevara Benites <wguevarab7@gmail.com>
2019-01-15 23:20:01 -0500

    cambios en el diseño

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,13 @@
 <template>
-	<div class="hello">
-		<div class="card-container">
+	<div class="hello" @click="showModal = !showModal">
+		<div class="card-container" >
 			<div class="card">
 				<div class="container-portada">
 					<div class="portada">
 					</div>
 				</div>
 				<div class="player"></div>
+				<div class="bomb"></div>
 				<div class="container-text">
 					<div class="text-one">
 						TE INVITO A MI
@@ -14,10 +15,25 @@
 					<div class="text-two">
 						BABYSHOWER
 					</div>
+					<div class="text-three">
+						CLICK PARA VER DETALLE
+					</div>
 				</div>
 				
 
 				<!-- <img src="../assets/bg_bomberman.png" alt=""> -->
+			</div>
+		</div>
+		<div class="modal" v-show="showModal">
+			<div class="card-modal">
+				<div>
+					<strong>Lugar:</strong>
+					<span>La ubicaciones del babyshower</span>
+				</div>
+				<div>
+					<strong>Lugar:</strong>
+					<span>La ubicaciones del babyshower</span>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -26,8 +42,17 @@
 <script>
 export default {
   name: 'HelloWorld',
+  data(){
+	  return {
+		  showModal: false
+	  }
+  },
+  methods: {
+	  showDetails: function() {
+		  console.log('detalles')
+	  }
+  }
   
-  bg_banner: require('../assets/bg_bomberman.png'),
 }
 </script>
 
@@ -45,12 +70,31 @@ export default {
 	align-items: center;
 	justify-content: center;
 }
+.modal{
+	position: absolute;
+    background: #00000078;
+    z-index: 99000;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.card-modal{
+	width: 280px;
+    background: #fff;
+    padding: 15px;
+    box-sizing: border-box;
+    border-radius: 4px;
+}
 .card-container{
-	min-width: 30rem;
+	cursor: pointer;
+	max-width: 30rem;
 	background: black;
-	padding:40px;
+	padding:55px;
 	box-sizing: border-box;
 	height: 500px;
+	width: 100%;
 }
 .card{
 	/* background: white; */
@@ -89,15 +133,67 @@ export default {
 		
 	}
 	&two{
-		// color: #fb8dcc;
 		color: #31dcff;
 		font-size: 60px;
+		transition: all 2s;
+		animation: textcolor 3s infinite;
+	}
+	&three{
+		color: white;
+	}
+}
+
+@media (max-width: 450px) {
+  .card-container {
+    max-width: 30rem;
+	height: 440px;
+	padding:35px;
+	.portada{
+		height: 305px;
+	}
+	.player{
+		top: 85px;
+		left: -20px;
+		position: absolute;
+		width: 200px;
+		height: 200px; 
+	
+	}
+	.container-text{
+		top: 269px;
+		.text-{
+			&one{
+				font-size:20px;
+			}
+			&two{
+				font-size:40px;
+			}
+			&three{
+				font-size: 15px;
+			}
+		}
+	}
+  }
+}
+
+@keyframes textcolor {
+	0%{
+		color: #ff6200;
+	}
+	30% {
+		color: #e5ec05;
+	}
+	60%{
+		color: #f642ff;
+	}
+	90% {
+		color: #31dcff;
 	}
 }
 .player{
 	background: url('../assets/bomberman.png');
-	top: 80px;
-    left: 8px;
+	top: 20px;
+    left: -22px;
     position: absolute;
     width: 270px;
     height: 270px;
@@ -105,4 +201,55 @@ export default {
     transform: scaleX(-1);
     z-index: 9999;
 }
+.bomb{
+	background: url('../assets/bomb.png');
+	top: 20px;
+    right: 8px;
+    position: absolute;
+    width: 140px;
+    height: 140px;
+    background-size: cover;
+	z-index: 9995;
+	animation: bomb 1s infinite; 
+}
+@keyframes bomb {
+	0% {
+		width: 140px;
+    	height: 140px;
+		top: 20px;
+    	right: 8px;
+	}
+	// 30% {
+	// 	width: 160px;
+    // 	height: 160px;
+		
+	// }
+	50% {
+		width: 160px;
+    	height: 160px;
+		top: 17px;
+    	right: 6px;
+		
+	}
+	// 60% {
+	// 	width: 140px;
+    // 	height: 140px;
+		
+	// }
+	100% {
+		width: 140px;
+    	height: 140px;
+		top: 20px;
+    	right: 8px;
+	}
+	// 90% {
+	// 	width: 140px;
+    // 	height: 140px;
+	// 	top: 20px;
+    // 	right: 8px;
+	// }
+}
+
+
+
 </style>
